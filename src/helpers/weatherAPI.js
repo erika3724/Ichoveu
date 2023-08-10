991834ae1d119414621eea7cfc245c5b2774448f
Andre <andre@pop-os.localdomain>
2023-08-10 20:12:09 -0300

    Requisito 01

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,15 @@
 // Remova os comentários a medida que for implementando as funções
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = (term) => {
+//   seu código aqui import.meta.env.VITE_TOKEN
+  return fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${import.meta.env.VITE_TOKEN}&q=${term}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.length === 0 || data.error) {
+        return window.alert('Nenhuma cidade encontrada', data);
+      }
+      return console.log(data);
+    });
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
